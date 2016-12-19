b967d4576d1b853e92d3329b4af7a8e618b20cb4
Donal McBreen <donal@freeagent.com>
2016-12-19 12:32:54 +0000

    Declare method before calling it

--- lib/capybara/webkit/server.rb
+++ lib/capybara/webkit/server.rb
@@ -4,21 +4,23 @@ require "thread"
 module Capybara
   module Webkit
     class Server
-      SERVER_PATH = find_webkit_server_path
-      WEBKIT_SERVER_START_TIMEOUT = 15
-
-      attr_reader :port, :pid
-
       def self.find_webkit_server_path
         paths_to_check = ["../../../../bin"]
         gemspec = Gem.loaded_specs["capybara-webkit"]
         paths_to_check.concat(gemspec.require_paths) if gemspec
 
-        paths_to_check do |path|
+        paths_to_check.each do |path|
           filepath = File.expand_path("#{path}/webkit_server", __FILE__)
           return filepath if File.exists?(filepath)
         end
+
+        raise "Could not find the webkit_server executable"
       end
+
+      SERVER_PATH = find_webkit_server_path
+      WEBKIT_SERVER_START_TIMEOUT = 15
+
+      attr_reader :port, :pid
 
       def initialize(options = {})
         if options.has_key?(:stderr)
